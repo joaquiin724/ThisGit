dde3e83e98bb518274753233e39764132ca7d48c
joaquiin_724 <joaquinrojo724@gmail.com>
2024-02-24 10:47:58 +0100

    Archivo JAVA

---
+++ HolaMundo.java
@@ -0,0 +1,12 @@
+// Programa que escribe en consola "Hola Mundo"
+// Autor: Joaquín Avilés de la Fuente
+// Fecha: 2024-02-24
+// Notas: Este programa es un ejemplo de un programa en Java
+// ----------------------------------------------------------
+// mostramos en consola "hola mundo"
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
