55f56e0fe511a6aefd8b9eb14413066acd304831
joaquiin724 <joaquinrojo724@gmail.com>
2024-03-08 11:46:02 +0100

    login.cpp

--- login.cpp
+++ login.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main(int argc, char *argv[]){
 
     cout << "Estoy en la rama \"login\" version 2 cambio" << endl;
+    cout << "Veamos un conflicto" << endl;
     return 0;
 }
